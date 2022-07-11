8626553525c4ffcc925ea83800f43fab95ba5632
Pete Dubler <pete.dubler@gmail.com>
2022-07-11 16:04:04 -0500

    LCD working with all outputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+// Pete Dulber mods:
+
+#define SERIAL_OUTPUT
+#define LCD_OUTPUT
+#define BLYNK_OUTPUT
+
 /*
  * I2C-Generator: 0.3.0
  * Yaml Version: 2.1.3
@@ -11,7 +17,7 @@
 */
 /*
  * Copyright (c) 2021, Sensirion AG
- * All rights reserved.
+ * All rights reserved.-
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -41,8 +47,19 @@
  */
 
 #include <Arduino.h>
+#include <heltec.h>  // 071122
 #include <SensirionI2CSen5x.h>
 #include <Wire.h>
+//#include "string.h"  // 071122
+
+float massConcentrationPm1p0;
+float massConcentrationPm2p5;
+float massConcentrationPm4p0;
+float massConcentrationPm10p0;
+float ambientHumidity;
+float ambientTemperature;
+float vocIndex;
+float noxIndex;
 
 // The used commands use up to 48 bytes. On some Arduino's the default buffer
 // space is not large enough
@@ -56,7 +73,8 @@
 
 SensirionI2CSen5x sen5x;
 
-void printModuleVersions() {
+void printModuleVersions()
+{
     uint16_t error;
     char errorMessage[256];
 
@@ -65,13 +83,16 @@ void printModuleVersions() {
 
     error = sen5x.getProductName(productName, productNameSize);
 
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute getProductName(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
-    } else {
+    }
+    else
+    {
         Serial.print("ProductName:");
-        Serial.println((char*)productName);
+        Serial.println((char *)productName);
     }
 
     uint8_t firmwareMajor;
@@ -85,11 +106,14 @@ void printModuleVersions() {
     error = sen5x.getVersion(firmwareMajor, firmwareMinor, firmwareDebug,
                              hardwareMajor, hardwareMinor, protocolMajor,
                              protocolMinor);
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute getVersion(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
-    } else {
+    }
+    else
+    {
         Serial.print("Firmware: ");
         Serial.print(firmwareMajor);
         Serial.print(".");
@@ -103,38 +127,61 @@ void printModuleVersions() {
     }
 }
 
-void printSerialNumber() {
+void printSerialNumber()
+{
     uint16_t error;
     char errorMessage[256];
     unsigned char serialNumber[32];
     uint8_t serialNumberSize = 32;
 
     error = sen5x.getSerialNumber(serialNumber, serialNumberSize);
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute getSerialNumber(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
-    } else {
+    }
+    else
+    {
         Serial.print("SerialNumber:");
-        Serial.println((char*)serialNumber);
+        Serial.println((char *)serialNumber);
     }
 }
 
-void setup() {
+void setup()
+{
+    // setup Heltec WIFI KIT 32  071122
+#ifdef LCD_OUTPUT
+    Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
+    //Heltec.display->flipScreenVertically();
+    Heltec.display->screenRotate(ANGLE_90_DEGREE);
+    Heltec.display->setFont(ArialMT_Plain_10);
+    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
+#endif
 
+    // We'll init serial in all cases just to have
+    //  for misc error messages and diagnostics  071122
     Serial.begin(115200);
-    while (!Serial) {
+    while (!Serial)
+    {
         delay(100);
     }
 
-    Wire.begin();
+
+#ifdef BLYNK_OUTPUT
+    // TODO  BLYNK and WIFI INIT
+#endif // #ifdef BLYNK_OUTPUT
+
+    // Wire.begin();  071122
+    Wire.begin();  // for secondary IC2  071122
 
     sen5x.begin(Wire);
 
     uint16_t error;
     char errorMessage[256];
     error = sen5x.deviceReset();
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute deviceReset(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
@@ -166,11 +213,14 @@ void setup() {
     // exceeding the SEN module's self heating.
     float tempOffset = 0.0;
     error = sen5x.setTemperatureOffsetSimple(tempOffset);
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute setTemperatureOffsetSimple(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
-    } else {
+    }
+    else
+    {
         Serial.print("Temperature Offset set to ");
         Serial.print(tempOffset);
         Serial.println(" deg. Celsius (SEN54/SEN55 only");
@@ -178,77 +228,147 @@ void setup() {
 
     // Start Measurement
     error = sen5x.startMeasurement();
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute startMeasurement(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
     }
 }
 
-void loop() {
+#ifdef SERIAL_OUTPUT
+void PrintResultsToSerial(void)
+{
+    Serial.print("MassConcentrationPm1p0:");
+    Serial.print(massConcentrationPm1p0);
+    Serial.print("\t");
+    Serial.print("MassConcentrationPm2p5:");
+    Serial.print(massConcentrationPm2p5);
+    Serial.print("\t");
+    Serial.print("MassConcentrationPm4p0:");
+    Serial.print(massConcentrationPm4p0);
+    Serial.print("\t");
+    Serial.print("MassConcentrationPm10p0:");
+    Serial.print(massConcentrationPm10p0);
+    Serial.print("\t");
+    Serial.print("AmbientHumidity:");
+    if (isnan(ambientHumidity))
+    {
+        Serial.print("n/a");
+    }
+    else
+    {
+        Serial.print(ambientHumidity);
+    }
+    Serial.print("\t");
+    Serial.print("AmbientTemperature:");
+    if (isnan(ambientTemperature))
+    {
+        Serial.print("n/a");
+    }
+    else
+    {
+        Serial.print(ambientTemperature);
+    }
+    Serial.print("\t");
+    Serial.print("VocIndex:");
+    if (isnan(vocIndex))
+    {
+        Serial.print("n/a");
+    }
+    else
+    {
+        Serial.print(vocIndex);
+    }
+    Serial.print("\t");
+    Serial.print("NoxIndex:");
+    if (isnan(noxIndex))
+    {
+        Serial.println("n/a");
+    }
+    else
+    {
+        Serial.println(noxIndex);
+    }
+}
+#endif // ifdef SERIAL_OUTPUT
+
+#ifdef LCD_OUTPUT
+void DisplayResultsToLCD(void)
+{
+    char tempstr[20];
+
+    Heltec.display->clear();
+
+    sprintf(tempstr,"Temp:   %2.1f", ambientTemperature);
+    Heltec.display->drawString(0   , 0  , tempstr);
+
+    sprintf(tempstr,"Humd:   %2.1f", ambientHumidity);
+    Heltec.display->drawString(0   , 16 , tempstr);
+
+    sprintf(tempstr,"Pm1.0:  %2.2f", massConcentrationPm1p0);
+    Heltec.display->drawString(0   , 32 , tempstr);
+
+    sprintf(tempstr,"Pm2.5:  %2.2f", massConcentrationPm2p5);
+    Heltec.display->drawString(0   , 48 , tempstr);
+
+    sprintf(tempstr,"Pm4.0:  %2.2f", massConcentrationPm4p0);
+    Heltec.display->drawString(0   , 64 , tempstr);
+
+    sprintf(tempstr,"Pm10:   %2.2f", massConcentrationPm10p0);
+    Heltec.display->drawString(0   , 80 , tempstr);
+
+    sprintf(tempstr,"VOC:    %2.1f", vocIndex);
+    Heltec.display->drawString(0   , 96 , tempstr);
+
+    Heltec.display->display();
+    delay(200);
+}
+#endif // #ifdef LCD_OUTPUT
+
+#ifdef BLYNK_OUTPUT
+void SendResultsToBlynk(void)
+{
+    //TODO   output data to Blynk
+}
+#endif // #ifdef BLYNK_OUTPUT
+
+
+void loop()
+{
     uint16_t error;
     char errorMessage[256];
 
     delay(1000);
 
     // Read Measurement
-    float massConcentrationPm1p0;
-    float massConcentrationPm2p5;
-    float massConcentrationPm4p0;
-    float massConcentrationPm10p0;
-    float ambientHumidity;
-    float ambientTemperature;
-    float vocIndex;
-    float noxIndex;
 
     error = sen5x.readMeasuredValues(
         massConcentrationPm1p0, massConcentrationPm2p5, massConcentrationPm4p0,
         massConcentrationPm10p0, ambientHumidity, ambientTemperature, vocIndex,
         noxIndex);
 
-    if (error) {
+    if (error)
+    {
         Serial.print("Error trying to execute readMeasuredValues(): ");
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
-    } else {
-        Serial.print("MassConcentrationPm1p0:");
-        Serial.print(massConcentrationPm1p0);
-        Serial.print("\t");
-        Serial.print("MassConcentrationPm2p5:");
-        Serial.print(massConcentrationPm2p5);
-        Serial.print("\t");
-        Serial.print("MassConcentrationPm4p0:");
-        Serial.print(massConcentrationPm4p0);
-        Serial.print("\t");
-        Serial.print("MassConcentrationPm10p0:");
-        Serial.print(massConcentrationPm10p0);
-        Serial.print("\t");
-        Serial.print("AmbientHumidity:");
-        if (isnan(ambientHumidity)) {
-            Serial.print("n/a");
-        } else {
-            Serial.print(ambientHumidity);
-        }
-        Serial.print("\t");
-        Serial.print("AmbientTemperature:");
-        if (isnan(ambientTemperature)) {
-            Serial.print("n/a");
-        } else {
-            Serial.print(ambientTemperature);
-        }
-        Serial.print("\t");
-        Serial.print("VocIndex:");
-        if (isnan(vocIndex)) {
-            Serial.print("n/a");
-        } else {
-            Serial.print(vocIndex);
-        }
-        Serial.print("\t");
-        Serial.print("NoxIndex:");
-        if (isnan(noxIndex)) {
-            Serial.println("n/a");
-        } else {
-            Serial.println(noxIndex);
-        }
+    }
+    else
+    {
+      #ifdef SERIAL_OUTPUT
+      PrintResultsToSerial();
+      #endif
+
+      #ifdef LCD_OUTPUT
+      DisplayResultsToLCD();
+      #endif
+
+      #ifdef BLYNK_OUTPUT
+      SendResultsToBlynk();
+      #endif
+
+
+
     }
 }
